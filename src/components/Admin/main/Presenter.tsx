8dab23d39f8e642bfe134c63fc0d9a54a9d1692d
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2021-09-30 14:00:44 +0900

    sidebarの実装完了

--- src/components/Admin/main/Presenter.tsx
+++ src/components/Admin/main/Presenter.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Presenter = () => {
   return (
     <div className="w-4/6 mx-auto my-10">
-      <span className="font-extrabold text-5xl pb-10">店舗住所aa</span>
+      <span className="font-extrabold text-5xl pb-10">店舗住所</span>
       <div className="pb-4" />
       <Divider sx={{ border: 1 }} className=" text-gray-300" />
     </div>
